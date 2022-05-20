7532237d67eb354ba71a9ba6013b429e3684b6d8
Mikhail <glader.ru@gmail.com>
2022-05-20 10:39:18 +0400

    Убрано ограничение на минимальную длину интро

--- users/forms/intro.py
+++ users/forms/intro.py
@@ -72,7 +72,6 @@ class UserIntroForm(ModelForm):
         widget=forms.Textarea(
             attrs={
                 "maxlength": 10000,
-                "minlength": 600,
                 "placeholder": "Расскажите Клубу о себе...",
             }
         ),
